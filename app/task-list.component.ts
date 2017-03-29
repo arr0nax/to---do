4a7934bd72588685ed9ecce93994ff5434d3abeb
Clayton Collins and Jennifer Beem <clayton@>
2017-03-29 09:57:46 -0700

    add completeness pipe

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -4,8 +4,16 @@ import { Task } from './task.model';
 @Component({
   selector: 'task-list',
   template: `
+  <select (change)="onChange($event.target.value)">
+    <option value="allTasks">All Tasks</option>
+    <option value="completedTasks">Completed Tasks</option>
+    <option value="incompleteTasks" selected="selected">Incomplete Tasks</option>
+  </select>
   <ul>
-    <li [class]="priorityColor(currentTask)" *ngFor="let currentTask of childTaskList">{{currentTask.description}} <button (click)="editButtonHasBeenClicked(currentTask)">Edit!</button></li>
+    <li (click)="isDone(currentTask)" [class]="priorityColor(currentTask)" *ngFor="let currentTask of childTaskList | completeness:filterByCompleteness">{{currentTask.description}} {{currentTask.priority}}
+    <input *ngIf="currentTask.done === true" type="checkbox" checked (click) ="toggleDone(currentTask, false)"/>
+    <input *ngIf="currentTask.done === false" type="checkbox" (click) ="toggleDone(currentTask, true)"/>
+    <button (click)="editButtonHasBeenClicked(currentTask)">Edit!</button></li>
   </ul>
   `
 })
@@ -14,8 +22,14 @@ export class TaskListComponent {
   @Input() childTaskList: Task[];
   @Output() clickSender = new EventEmitter();
 
+  filterByCompleteness: string = "incompleteTasks";
+
   editButtonHasBeenClicked(tasktoEdit: Task) {
     this.clickSender.emit(tasktoEdit);
+  }
+
+  onChange(optionFromMenu) {
+    this.filterByCompleteness = optionFromMenu;
   }
 
   priorityColor(currentTask){
@@ -25,6 +39,17 @@ export class TaskListComponent {
       return "bg-warning";
     } else {
       return "bg-info";
+    }
+  }
+  toggleDone(clickedTask: Task, setCompleteness: boolean) {
+    clickedTask.done = setCompleteness;
+  }
+
+  isDone(currentTask) {
+    if (currentTask.done === true) {
+      currentTask.done = false;
+    } else {
+      currentTask.done = true;
     }
   }
 }
